b049d9f52de0f4a5cc147a72de6e62d7fc04f71a
Demezhan Kazhkenov <59953572+dropdead619@users.noreply.github.com>
2020-12-30 19:27:13 +0600

    Tanks commit

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7__Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] PantherDivision = { new Tank("Panther"), new Tank("Panther"), new Tank("Panther"), new Tank("Panther"), new Tank("Panther") };
+            Tank[] T34Division = { new Tank("T-34"), new Tank("T-34"), new Tank("T-34"), new Tank("T-34"), new Tank("T-34") };
+            int countOfFights = PantherDivision.Length;
+            for (int i = 0; i < countOfFights; i++)
+            {
+                Console.WriteLine("Fight number " + (i + 1) + "\n");
+                if (PantherDivision[i] ^ T34Division[i])
+                {
+                    Console.WriteLine($"Tank Panther with:\n" +
+                        $"Ammunition Level: {PantherDivision[i].GetAmmunitionLevelString()}\n" +
+                        $"Level of armor: {PantherDivision[i].GetArmorLevelString()}\n" +
+                        $"Maneuverability Level: {PantherDivision[i].GetManeuverabilityLevelString()}\n" +
+                        $"Won in this fight!\n");
+                    Console.WriteLine($"Tank T-34 with:\n" +
+                        $"Ammunition Level: {T34Division[i].GetAmmunitionLevelString()}\n" +
+                        $"Level of armor: {T34Division[i].GetArmorLevelString()}\n" +
+                        $"Maneuverability Level: {T34Division[i].GetManeuverabilityLevelString()}\n" +
+                        $"Lost in this fight!");
+                }
+                else
+                {
+                    Console.WriteLine($"Tank T-34 with:\n" +
+                        $"Ammunition Level: {T34Division[i].GetAmmunitionLevelString()}\n" +
+                        $"Level of armor: {T34Division[i].GetArmorLevelString()}\n" +
+                        $"Maneuverability Level: {T34Division[i].GetManeuverabilityLevelString()}\n" +
+                        $"Won in this fight!\n");
+                    Console.WriteLine($"Tank Panther with:\n" +
+                        $"Ammunition Level: {PantherDivision[i].GetAmmunitionLevelString()}\n" +
+                        $"Level of armor: {PantherDivision[i].GetArmorLevelString()}\n" +
+                        $"Maneuverability Level: {PantherDivision[i].GetManeuverabilityLevelString()}\n" +
+                        $"Lost in this fight!");
+                };
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private string name;
+        private int ammunitionLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+        public Tank(string name)
+        {
+            this.name = name;
+            Random rnd = new Random();
+            ammunitionLevel = rnd.Next(0, 100);
+            armorLevel = rnd.Next(0, 100);
+            maneuverabilityLevel = rnd.Next(0, 100);
+        }
+        public static bool operator ^(Tank firstTank, Tank secondTank)
+        {
+            if ((firstTank.ammunitionLevel == secondTank.ammunitionLevel) &&
+                     (firstTank.armorLevel == secondTank.armorLevel) &&
+                     (firstTank.maneuverabilityLevel == secondTank.maneuverabilityLevel))
+            {
+                return new Random().Next() > (Int32.MaxValue / 2);
+            }
+            else if ((firstTank.ammunitionLevel == secondTank.ammunitionLevel) &&
+                     (firstTank.armorLevel == secondTank.armorLevel) &&
+                     (firstTank.maneuverabilityLevel > secondTank.maneuverabilityLevel) ||
+                     (firstTank.ammunitionLevel == secondTank.ammunitionLevel) &&
+                     (firstTank.armorLevel > secondTank.armorLevel) &&
+                     (firstTank.maneuverabilityLevel == secondTank.maneuverabilityLevel) ||
+                     (firstTank.ammunitionLevel > secondTank.ammunitionLevel) &&
+                     (firstTank.armorLevel == secondTank.armorLevel) &&
+                     (firstTank.maneuverabilityLevel == secondTank.maneuverabilityLevel)
+                    )
+            {
+                return true;
+            }
+            else if ((firstTank.ammunitionLevel > secondTank.ammunitionLevel &&
+              firstTank.armorLevel > secondTank.armorLevel) ||
+              (firstTank.armorLevel > secondTank.armorLevel &&
+               firstTank.maneuverabilityLevel > secondTank.maneuverabilityLevel) ||
+              (firstTank.maneuverabilityLevel > secondTank.maneuverabilityLevel &&
+              firstTank.ammunitionLevel > secondTank.ammunitionLevel)
+              )
+            {
+                return true;
+            }
+            return false;
+        }
+        public string GetAmmunitionLevelString()
+        {
+            return ammunitionLevel.ToString();
+        }
+        public string GetArmorLevelString()
+        {
+            return armorLevel.ToString();
+        }
+        public string GetManeuverabilityLevelString()
+        {
+            return maneuverabilityLevel.ToString();
+        }
+    }
+}
